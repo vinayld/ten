e2a4dd7c1bcdcf7c034b29e882b91b519bccbb86
vinayld <vinayld05@gmail.com>
2022-11-25 10:43:02 +0530

    this is first commit

---
+++ src/ten/two.java
@@ -0,0 +1,13 @@
+package ten;
+
+public class two {
+
+	public static void main(String[] args) {
+		int i=2;
+		String s= "vinay";
+		System.out.println(i);
+		System.out.println("vinay");
+
+	}
+
+}
